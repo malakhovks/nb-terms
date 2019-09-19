365adf26127a03fe88f7fb2f3788ede72062291f
Kirill Malakhov <malakhovks@nas.gov.ua>
2019-09-19 10:33:17 +0300

    Update and setup project
    Update and setup project

---
+++ server.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# load tempfile for temporary dir creation
+import sys, os, tempfile
+
+import json
+
+# load libraries for NLP pipeline
+import spacy
+
+# load libraries for API proccessing
+from flask import Flask, jsonify, flash, request, Response, redirect, url_for, abort, render_template
+# A Flask extension for handling Cross Origin Resource Sharing (CORS), making cross-origin AJAX possible.
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+
+# Load globally spaCy model via package name
+# NLP_EN = spacy.load('en_core_web_lg')
+
+class JSONResponse(Response):
+    default_mimetype = 'application/json'
+
+app = Flask(__name__)
+CORS(app)
+app.response_class = JSONResponse
+
+"""
+Limited the maximum allowed payload to 16 megabytes.
+If a larger file is transmitted, Flask will raise an RequestEntityTooLarge exception.
+"""
+# app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+"""
+Set the secret key to some random bytes. Keep this really secret!
+How to generate good secret keys.
+A secret key should be as random as possible. Your operating system has ways to generate pretty random data based on a cryptographic random generator. Use the following command to quickly generate a value for Flask.secret_key (or SECRET_KEY):
+$ python -c 'import os; print(os.urandom(16))'
+b'_5#y2L"F4Q8z\n\xec]/'
+"""
+# app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.secret_key = os.urandom(42)
+
+@app.route('/')
+def index():
+    return Response(render_template('index.html'), mimetype='text/html')
+
+@app.route('/cvparsing')
+def cvp():
+    return Response(render_template('cvparsing.html'), mimetype='text/html')
+
+@app.route('/cvwe')
+def cvwe():
+    return Response(render_template('cvwe.html'), mimetype='text/html')
+
+if __name__ == '__main__':
+    # default port = 5000
+    app.run(host = '0.0.0.0')
+    # app.run(host = '127.0.0.1', port = 8000)
