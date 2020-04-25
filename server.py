41711abe1f1b0c1e6f9222f20ac953a19a4f2e52
Kirill Malakhov <malakhovks@nas.gov.ua>
2020-04-25 12:35:29 +0300

    Added parce.xml for Norwegian Bokmål
    Added parce.xml for Norwegian Bokmål

--- server.py
+++ server.py
@@ -8,6 +8,8 @@ import json
 
 # load libraries for NLP pipeline
 import spacy
+# load Visualizers 
+from spacy import displacy
 
 # load misc utils
 import uuid
@@ -36,15 +38,18 @@ import xml.etree.ElementTree as ET
 ALLOWED_EXTENSIONS = set(['docx']) 
 
 # Load globally spaCy model via package name
-# NLP_EN = spacy.load('en_core_web_sm')
+NLP_NB = spacy.load('nb_core_news_sm')
 NLP_EN_VECTORES = spacy.load('en_core_web_lg')
 
-class JSONResponse(Response):
-    default_mimetype = 'application/json'
+__author__ = "Kyrylo Malakhov <malakhovks@nas.gov.ua> and Vitalii Velychko <aduisukr@gmail.com>"
+__copyright__ = "Copyright (C) 2020 Kyrylo Malakhov <malakhovks@nas.gov.ua> and Vitalii Velychko <aduisukr@gmail.com>"
+
+# class JSONResponse(Response):
+#     default_mimetype = 'application/json'
 
 app = Flask(__name__)
 CORS(app)
-app.response_class = JSONResponse
+# app.response_class = JSONResponse
 
 """
 Limited the maximum allowed payload to 16 megabytes.
@@ -360,7 +365,8 @@ def get_data_from_cv_json():
                 file.save(destination)
                 file.close()
                 if os.path.isfile(destination):
-                    return get_data_from_ridge_docx_into_json(destination)
+                    # return get_data_from_ridge_docx_into_json(destination)
+                    return Response(get_data_from_ridge_docx_into_json(destination), mimetype='application/json')
             except Exception as e:
                 logging.error(e, exc_info=True)
             return abort(500)
@@ -388,7 +394,8 @@ def get_data_from_cv_xml():
             file.save(destination)
             file.close()
             if os.path.isfile(destination):
-                return docx_ridge_to_xml(destination, True)
+                # return docx_ridge_to_xml(destination, True)
+                return Response(docx_ridge_to_xml(destination, True), mimetype='text/xml')
         except Exception as e:
             logging.error(e, exc_info=True)
         return abort(500)
@@ -425,14 +432,146 @@ def get_data_from_cvs_xml():
                             xml_final_tree.find('Nodes').append(el_Node)
                     for el_Edge in docx_ridge_to_xml(destination, False).iter('Edge'):
                         xml_final_tree.find('Edges').append(el_Edge)
-    return ET.tostring(xml_final_tree, encoding='unicode', method='xml')
+    return Response(ET.tostring(xml_final_tree, encoding='unicode', method='xml'), mimetype='text/xml')
 
 @app.route('/wv/api/en/similarity', methods=['POST'])
 def get_similarity():
     req_data = request.get_json()
     doc1 = NLP_EN_VECTORES(req_data['text1'])
     doc2 = NLP_EN_VECTORES(req_data['text2'])
-    return jsonify(similarity = doc1.similarity(doc2))
+    return Response(jsonify(similarity = doc1.similarity(doc2)), mimetype='application/json')
+
+# --------------------------------------------------------------------------------------------------------
+@app.route('/api/bot/nb/allterms', methods=['POST'])
+def get_allterms():
+    req_data = request.get_json()
+
+@app.route('/api/bot/nb/parcexml', methods=['POST'])
+def get_parcexml():
+    # POS UD
+    # https://universaldependencies.org/u/pos/
+    if (request.args.get('pos', None) == 'ud') or (request.args.get('pos', None) == None):
+        speech_dict_POS_tags = {'NOUN':'S1', 'ADJ':'S2', 'VERB': 'S4', 'INTJ':'S21', 'PUNCT':'98', 'SYM':'98', 'CONJ':'U', 'NUM':'S7', 'X':'99', 'PRON':'S11', 'ADP':'P', 'PROPN':'S22', 'ADV':'S16', 'AUX':'99', 'CCONJ':'U', 'DET':'99', 'PART':'99', 'SCONJ':'U', 'SPACE':'98'}
+    # TODO Correctly relate the parts of speech with spaCy
+    # POS spaCy
+    if request.args.get('pos', None) == 'spacy':
+        speech_dict_POS_tags = {'NOUN':'S1', 'ADJ':'S2', 'VERB': 'S4', 'INTJ':'S21', 'PUNCT':'98', 'SYM':'98', 'CONJ':'U', 'NUM':'S7', 'X':'S29', 'PRON':'S10', 'ADP':'P', 'PROPN':'S22', 'ADV':'S16', 'AUX':'AUX', 'CCONJ':'CCONJ', 'DET':'DET', 'PART':'PART', 'SCONJ':'SCONJ', 'SPACE':'SPACE'}
+
+    req_data = request.get_json()
+
+    try:
+        doc = NLP_NB(req_data['message'])
+        """
+        # create the <parce.xml> file structure
+        """
+        # create root element <text>
+        root_element = ET.Element("text")
+
+        for sentence_index, sentence in enumerate(doc.sents):
+            # XML structure creation
+            new_sentence_element = ET.Element('sentence')
+            # create and append <sentnumber>
+            new_sentnumber_element = ET.Element('sentnumber')
+            new_sentnumber_element.text = str(sentence_index+1)
+            new_sentence_element.append(new_sentnumber_element)
+            # create and append <sent>
+            new_sent_element = ET.Element('sent')
+            new_sent_element.text = sentence.text
+            new_sentence_element.append(new_sent_element)
+
+            doc_for_lemmas = NLP_NB(sentence.text)
+
+            # create amd append <ner>, <entity>
+            # NER labels description https://spacy.io/api/annotation#named-entities
+            if len(doc_for_lemmas.ents) != 0:
+                # create <ner>
+                ner_element = ET.Element('ner')
+                for ent in doc_for_lemmas.ents:
+                    # create <entity>
+                    new_entity_element = ET.Element('entity')
+                    # create and append <entitytext>
+                    new_entity_text_element = ET.Element('entitytext')
+                    new_entity_text_element.text = ent.text
+                    new_entity_element.append(new_entity_text_element)
+                    # create and append <label>
+                    new_entity_label_element = ET.Element('label')
+                    new_entity_label_element.text = ent.label_
+                    new_entity_element.append(new_entity_label_element)
+                    # create and append <startentitypos>
+                    new_start_entity_pos_character_element = ET.Element('startentityposcharacter')
+                    new_start_entity_pos_token_element = ET.Element('startentitypostoken')
+                    new_start_entity_pos_character_element.text = str(ent.start_char + 1)
+                    new_start_entity_pos_token_element.text = str(ent.start + 1)
+                    new_entity_element.append(new_start_entity_pos_character_element)
+                    new_entity_element.append(new_start_entity_pos_token_element)
+                    # create and append <endentitypos>
+                    new_end_entity_pos_character_element = ET.Element('endentityposcharacter')
+                    new_end_entity_pos_token_element = ET.Element('endentitypostoken')
+                    new_end_entity_pos_character_element.text = str(ent.end_char)
+                    new_end_entity_pos_token_element.text = str(ent.end)
+                    new_entity_element.append(new_end_entity_pos_character_element)
+                    new_entity_element.append(new_end_entity_pos_token_element)
+                    # append <entity> to <ner>
+                    ner_element.append(new_entity_element)
+                # append <ner> to <sentence>
+                new_sentence_element.append(ner_element)
+            # create and append <item>, <word>, <lemma>, <number>, <pos>, <speech>
+            for lemma in doc_for_lemmas:
+                # create and append <item>
+                new_item_element = ET.Element('item')
+                # create and append <word>
+                new_word_element = ET.Element('word')
+                new_word_element.text = lemma.text
+                new_item_element.append(new_word_element)
+                # create and append <lemma>
+                new_lemma_element = ET.Element('lemma')
+                new_lemma_element.text = lemma.lemma_ #.encode('ascii', 'ignore')
+                new_item_element.append(new_lemma_element)
+                # create and append <number>
+                new_number_element = ET.Element('number')
+                new_number_element.text = str(lemma.i+1)
+                new_item_element.append(new_number_element)
+                # create and append <speech>
+                new_speech_element = ET.Element('speech')
+                # relate the universal dependencies parts of speech with konspekt tags
+                if (request.args.get('pos', None) == 'ud') or (request.args.get('pos', None) == None):
+                    new_speech_element.text = speech_dict_POS_tags[lemma.pos_]
+                # relate the spaCy parts of speech with konspekt tags
+                if request.args.get('pos', None) == 'spacy':
+                    new_speech_element.text = speech_dict_POS_tags[lemma.tag_]
+                new_item_element.append(new_speech_element)
+                # create and append <pos>
+                new_pos_element = ET.Element('pos')
+                new_pos_element.text = str(lemma.idx+1)
+                new_item_element.append(new_pos_element)
+
+                # create and append <relate> and <rel_type>
+                new_rel_type_element = ET.Element('rel_type')
+                new_relate_element = ET.Element('relate')
+                if lemma.dep_ == 'punct':
+                    new_rel_type_element.text = 'K0'
+                    new_relate_element.text = '0'
+                    new_item_element.append(new_rel_type_element)
+                    new_item_element.append(new_relate_element)
+                else:
+                    new_rel_type_element.text = lemma.dep_
+                    new_item_element.append(new_rel_type_element)
+                    new_relate_element.text = str(lemma.head.i+1)
+                    new_item_element.append(new_relate_element)
+
+                # create and append <group_n>
+                new_group_n_element = ET.Element('group_n')
+                new_group_n_element.text = '1'
+                new_item_element.append(new_group_n_element)
+
+                new_sentence_element.append(new_item_element)
+            # create full <parce.xml> file structure
+            root_element.append(new_sentence_element)
+        return Response(ET.tostring(root_element, encoding='utf8', method='xml'), mimetype='text/xml')
+    except Exception as e:
+        logging.error(e, exc_info=True)
+        return abort(500)
+# --------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     # default port = 5000
