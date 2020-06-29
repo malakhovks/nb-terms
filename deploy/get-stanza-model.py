11c4b51da4163f756e1ff17dd4baf197112dde93
Kirill Malakhov <malakhovks@nas.gov.ua>
2020-06-29 22:18:27 +0300

    format code
    format code

--- deploy/get-stanza-model.py
+++ deploy/get-stanza-model.py
@@ -1,2 +1,6 @@
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 import stanza
-stanza.download('nb')
+logging.debug('Installing Stance pretrained NLP model for Norwegian Bokmaal.')
+stanza.download('nb', dir='./deploy/stanza_resources')
+logging.debug('Stance pretrained NLP model for Norwegian Bokmaal is ready to use.')
