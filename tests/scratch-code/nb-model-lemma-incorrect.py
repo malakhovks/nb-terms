000fca4fca821804f3a0c6eff428cef06538b55a
Kirill Malakhov <malakhovks@nas.gov.ua>
2020-06-29 11:36:34 +0300

    update with spaCy lemma test
    update with spaCy lemma test

--- tests/scratch-code/nb-model-lemma-incorrect.py
+++ tests/scratch-code/nb-model-lemma-incorrect.py
@@ -2,7 +2,12 @@ import spacy
 
 nlp = spacy.load("nb_core_news_sm")
 doc = nlp("Formuesskatten er en skatt som utlignes på grunnlag av nettoformuen din.")
+doc1 = nlp("Formuess")
 
 for token in doc:
+    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+            token.shape_, token.is_alpha, token.is_stop)
+
+for token in doc1:
     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
             token.shape_, token.is_alpha, token.is_stop)
