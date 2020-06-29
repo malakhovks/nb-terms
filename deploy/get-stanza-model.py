9f193f8752068574c6440b4387c3dfe2bb6de724
Kirill Malakhov <malakhovks@nas.gov.ua>
2020-06-29 22:18:45 +0300

    Revert "format code"
    This reverts commit 11c4b51da4163f756e1ff17dd4baf197112dde93.

--- deploy/get-stanza-model.py
+++ deploy/get-stanza-model.py
@@ -1,6 +1,2 @@
-import logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 import stanza
-logging.debug('Installing Stance pretrained NLP model for Norwegian Bokmaal.')
-stanza.download('nb', dir='./deploy/stanza_resources')
-logging.debug('Stance pretrained NLP model for Norwegian Bokmaal is ready to use.')
+stanza.download('nb')
